c463580ef6650613086b973440c1320df11892f2
malmeshnik <andriyvasiliev321@gmail.com>
2025-06-02 10:40:50 +0300

    refactor

--- traffic_project/traffic_monitor/admin.py
+++ traffic_project/traffic_monitor/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import VideoUpload, DetectionResult, AggregatedData
+
 
 # Register your models here.
+@admin.register(VideoUpload)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'video_file', 'status', 'uploaded_at', 'processed_at')
+    search_fields = ('video_file',)
+    list_filter = ('uploaded_at',)
+
+@admin.register(DetectionResult)
+class DetectionResultAdmin(admin.ModelAdmin):
+    list_display = ('video', 'timestamp_in_video', 'vehicle_class', 'count')
+    search_fields = ('video__title', 'vehicle_class')
+    list_filter = ('vehicle_class',)
+
+@admin.register(AggregatedData)
+class AggregatedDataAdmin(admin.ModelAdmin):
+    list_display = ('video', 'time_period_start', 'vehicle_class', 'count')
+    search_fields = ('video__title', 'vehicle_class')
+    list_filter = ('vehicle_class',)
+
